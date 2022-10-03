db365371f6ad7c24ab3f90feab536b5ced4e4f25
Cam Mullen <cameron.mullen@gmail.com>
2022-10-03 16:39:02 -0700

    Implement loading of SaasData

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
-use std::collections::HashMap;
-use std::collections::hash_map::Entry::{Occupied, Vacant};
 use chrono::naive::NaiveDate;
 use serde::Deserialize;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
+use std::collections::HashMap;
 
 pub struct Contract {
     customer_id: String,
     start_date: NaiveDate,
     end_date: NaiveDate,
     tcv: f64,
+    acv: f64,
+}
+
+enum ContractEvent<'a> {
+    Start(&'a Contract),
+    End(&'a Contract),
 }
 
 pub struct Customer {
@@ -24,12 +30,14 @@ pub struct ContractRecord {
 }
 
 pub struct SaasData {
-    customers: HashMap<String, Customer>
+    customers: HashMap<String, Customer>,
 }
 
 impl SaasData {
     pub fn new<T: IntoIterator<Item = ContractRecord>>(data_iter: T) -> Self {
-        let mut data = SaasData { customers: HashMap::new() };
+        let mut data = SaasData {
+            customers: HashMap::new(),
+        };
         for record in data_iter {
             data.add_contract_record(record);
         }
@@ -51,7 +59,10 @@ impl SaasData {
 
 impl Customer {
     fn new(customer_id: &str) -> Self {
-        Customer {id: customer_id.to_string(), contracts: vec![] }
+        Customer {
+            id: customer_id.to_string(),
+            contracts: vec![],
+        }
     }
 
     fn add_contract_record(&mut self, record: ContractRecord) {
@@ -59,15 +70,21 @@ impl Customer {
     }
 }
 
-
-
 impl From<ContractRecord> for Contract {
-    fn from(r : ContractRecord) -> Self {
-        Contract { customer_id: r.customer_id, start_date: r.start_date, end_date: r.end_date, tcv: r.tcv }
+    fn from(r: ContractRecord) -> Self {
+        let contract_dur_days = (r.end_date - r.start_date).num_days() as f64;
+        // Round to 1 decimal place
+        let contract_dur_years: f64 = (contract_dur_days * 10.0 / 365.0).round() / 10.0;
+        let acv = r.tcv / contract_dur_years;
+        Contract {
+            customer_id: r.customer_id,
+            start_date: r.start_date,
+            end_date: r.end_date,
+            tcv: r.tcv,
+            acv,
+        }
     }
 }
-
-
 
 // impl<'a> Customer<'a> {
 //     pub fn new(id: String) -> Self {
